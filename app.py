a184f0424227ecde6b028f7485f5e8fd5e4a75c1
Magordini <miguel.g3255@gmail.com>
2021-09-20 23:30:12 -0500

    Corrigiendo anterior commit

--- app.py
+++ app.py
@@ -64,39 +64,6 @@ def iniciar_sesion():
         return render_template("inicioSesion.html")
     except:    
         return render_template("inicioSesion.html")
-    if request.method == "POST":
-        username=request.form["user"]
-        correo=request.form["email"]
-        password=request.form["password"]
-        error=None
-        if not ut.isUsernameValid(username):
-            error="El usuario es invalido"
-            flash(error)
-            render_template("/registro.html")
-
-        if not ut.isEmailValid(correo):
-            error="el correo no es valido"
-            flash(error)
-            render_template("/registro.html")
-
-        if not ut.isPasswordValid(password):
-            error="La contraseña debe tener por lo menos una minuscula, una mayuscula, un numero y 8 caracteres"
-            flash(error)
-            render_template("/registro.html")
-
-        Result=mod.ingresar_usuario(username,correo,password)
-        print(Result)
-        if Result != "Creacion exitosa":
-            flash(Result)
-            return render_template("/registro.html")
-        else:
-            return redirect("/")
-
-    return render_template("registro.html")
-
-@app.route("/iniciar_sesion")
-def iniciar_sesion():
-    return render_template("inicioSesion.html")
 
 @app.route("/detalle")
 def detalles():
